b1492283dd329f9e57fa78224d511cbead7eecca
Danil Gasimov <dengad2003@yandex.ru>
2025-05-04 22:35:01 +0300

    commit

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from selenium.webdriver.chrome.options import Options
 import time
 import datetime
 from pathlib import Path
-
+import csv
 
 
 def get_online_stats(url: str) -> int:
-    options = Options()
-    options.add_argument("--headless")
     try:
+        options = Options()
+        options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
         driver.get(url)
         time.sleep(3)
@@ -21,25 +21,28 @@ def get_online_stats(url: str) -> int:
     finally:
         driver.quit()
 
+    game_name = str(soup.find('div', class_='apphub_AppDetails').find('div', class_='apphub_AppName ellipsis').text)
     div_online_stats = soup.find('div', class_='apphub_Stats').find('span', class_='apphub_NumInApp')
     online_stats = int(div_online_stats.text.replace(',','').replace(' в игре', ''))
-    print(online_stats)
+    print(f"Name: {game_name}, Online: {online_stats}")
     
-    return online_stats
+    return game_name, online_stats
 
 
-def write_online_stats_to_csv(online_stats):
+def write_online_stats_to_csv(game_name, online_stats):
     online_stats_data = {
-        'time' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-        'player_count' : online_stats
+        'name' : f"{game_name}",
+        'player_count' : online_stats,
+        'time' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     }
     file = Path('online_stats.csv')
     df = pd.DataFrame([online_stats_data])
-    df.to_csv('online_stats.csv', index=False, mode='a', header=not file.exists())
+    df.to_csv('online_stats.csv', index=False, mode='a', header=not file.exists(), quoting=csv.QUOTE_NONNUMERIC)
+
 
 def main():
-    a = get_online_stats("https://steamcommunity.com/app/730")
-    write_online_stats_to_csv(a)
+    game_name, online_stats = get_online_stats("https://steamcommunity.com/app/730")
+    write_online_stats_to_csv(game_name, online_stats)
 
 if __name__ == "__main__":
     main()
